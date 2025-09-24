8e5e2951b691ef8aa8510de584d548c6dc067f4d
Joan Boure <jmboure@gmail.com>
2025-09-24 22:02:51 +0200

    initial commit

---
+++ build_markdown.py
@@ -0,0 +1,77 @@
+import gpxpy
+import matplotlib.pyplot as plt
+import contextily as ctx
+import pyproj
+import os
+
+def gpx_to_png(gpx_file, png_file):
+    with open(gpx_file, 'r') as f:
+        gpx = gpxpy.parse(f)
+    lats, lons, alts = [], [], []
+    wp_lats, wp_lons = [], []
+    total_distance = 0.0
+    total_ascent = 0.0
+
+    for track in gpx.tracks:
+        for segment in track.segments:
+            prev_point = None
+            for point in segment.points:
+                lats.append(point.latitude)
+                lons.append(point.longitude)
+                alts.append(point.elevation)
+                if prev_point:
+                    total_distance += point.distance_3d(prev_point)
+                    delta_alt = point.elevation - prev_point.elevation
+                    if delta_alt > 0:
+                        total_ascent += delta_alt
+                prev_point = point
+
+    # Waypoints
+    for wp in gpx.waypoints:
+        wp_lats.append(wp.latitude)
+        wp_lons.append(wp.longitude)
+
+    # Convertir a Web Mercator (EPSG:3857)
+    proj = pyproj.Transformer.from_crs("epsg:4326", "epsg:3857", always_xy=True)
+    x, y = proj.transform(lons, lats)
+    if wp_lons and wp_lats:
+        x_wp, y_wp = proj.transform(wp_lons, wp_lats)
+    else:
+        x_wp, y_wp = [], []
+
+    plt.figure(figsize=(10, 8))
+    plt.plot(x, y, 'b-', linewidth=2)
+    if x_wp and y_wp:
+        plt.scatter(x_wp, y_wp, marker='*', color='red', s=150, label='Waypoints')
+    plt.axis('equal')
+    plt.axis('off')
+    ax = plt.gca()
+    ctx.add_basemap(ax, crs="epsg:3857", source=ctx.providers.OpenStreetMap.Mapnik)
+
+    filename = os.path.basename(gpx_file)
+    km = total_distance / 1000
+    ascent = total_ascent
+    text = f"{filename}\nDistancia: {km:.1f} km\nAscenso acumulado: {ascent:.0f} m"
+    plt.text(0.01, 0.99, text, transform=ax.transAxes, fontsize=12,
+             verticalalignment='top', bbox=dict(facecolor='white', alpha=0.7, edgecolor='none'))
+
+    plt.savefig(png_file, bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+# Procesar todos los archivos GPX en la carpeta 'gpx' y generar motoriders.md
+gpx_folder = 'gpx'
+md_filename = 'motoriders.md'
+entries = []
+
+for filename in os.listdir(gpx_folder):
+    if filename.lower().endswith('.gpx'):
+        gpx_path = os.path.join(gpx_folder, filename)
+        png_path = os.path.splitext(gpx_path)[0] + '.png'
+        gpx_to_png(gpx_path, png_path)
+        name = os.path.splitext(filename)[0]
+        entry = f"### {name}\n\n![{name}]({png_path})\n\n[Descargar GPX]({gpx_path})\n"
+        entries.append(entry)
+
+with open(md_filename, 'w', encoding='utf-8') as md_file:
+    md_file.write("# Motoriders\n\n")
+    md_file.write("\n---\n".join(entries))
---
+++ motoriders.md
@@ -0,0 +1,105 @@
+# Motoriders
+
+### Ducati dosmares.2023
+
+![Ducati dosmares.2023](gpx\Ducati dosmares.2023.png)
+
+[Descargar GPX](gpx\Ducati dosmares.2023.gpx)
+
+---
+### Ducati dosmares.2024
+
+![Ducati dosmares.2024](gpx\Ducati dosmares.2024.png)
+
+[Descargar GPX](gpx\Ducati dosmares.2024.gpx)
+
+---
+### Eutichesbook 500 pro.2025
+
+![Eutichesbook 500 pro.2025](gpx\Eutichesbook 500 pro.2025.png)
+
+[Descargar GPX](gpx\Eutichesbook 500 pro.2025.gpx)
+
+---
+### Rider 1000.2014
+
+![Rider 1000.2014](gpx\Rider 1000.2014.png)
+
+[Descargar GPX](gpx\Rider 1000.2014.gpx)
+
+---
+### Rider 1000.2015
+
+![Rider 1000.2015](gpx\Rider 1000.2015.png)
+
+[Descargar GPX](gpx\Rider 1000.2015.gpx)
+
+---
+### Rider 1000.2016
+
+![Rider 1000.2016](gpx\Rider 1000.2016.png)
+
+[Descargar GPX](gpx\Rider 1000.2016.gpx)
+
+---
+### Rider 1000.2017
+
+![Rider 1000.2017](gpx\Rider 1000.2017.png)
+
+[Descargar GPX](gpx\Rider 1000.2017.gpx)
+
+---
+### Rider 1000.2018
+
+![Rider 1000.2018](gpx\Rider 1000.2018.png)
+
+[Descargar GPX](gpx\Rider 1000.2018.gpx)
+
+---
+### Rider 1000.2019
+
+![Rider 1000.2019](gpx\Rider 1000.2019.png)
+
+[Descargar GPX](gpx\Rider 1000.2019.gpx)
+
+---
+### Rider 1000.2020
+
+![Rider 1000.2020](gpx\Rider 1000.2020.png)
+
+[Descargar GPX](gpx\Rider 1000.2020.gpx)
+
+---
+### Rider 1000.2021
+
+![Rider 1000.2021](gpx\Rider 1000.2021.png)
+
+[Descargar GPX](gpx\Rider 1000.2021.gpx)
+
+---
+### Rider 1000.2022
+
+![Rider 1000.2022](gpx\Rider 1000.2022.png)
+
+[Descargar GPX](gpx\Rider 1000.2022.gpx)
+
+---
+### Rider 1000.2023
+
+![Rider 1000.2023](gpx\Rider 1000.2023.png)
+
+[Descargar GPX](gpx\Rider 1000.2023.gpx)
+
+---
+### Rider 1000.2024
+
+![Rider 1000.2024](gpx\Rider 1000.2024.png)
+
+[Descargar GPX](gpx\Rider 1000.2024.gpx)
+
+---
+### Rider 1000.2025
+
+![Rider 1000.2025](gpx\Rider 1000.2025.png)
+
+[Descargar GPX](gpx\Rider 1000.2025.gpx)
